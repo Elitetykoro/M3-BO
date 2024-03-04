de3d2a8e44a402d75ec57a878c129c4d89dd96d8
Elitetykoro <tykoromathijssen@gmail.com>
2024-03-04 20:30:10 +0100

    EnemyMovement

--- Assets/Scenes/Scripts/enemySpawner.cs
+++ Assets/Scenes/Scripts/enemySpawner.cs
@@ -6,7 +6,11 @@ public class enemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject enemy;
+    [SerializeField] Transform player;
     [SerializeField] Transform Spawner;
+    public Vector3 velocity = new Vector3 ();
+    private float z;
+    private float x;
     void Start()
     {
         StartCoroutine("enemySpawn");
@@ -18,12 +22,32 @@ public class enemySpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(4f, 10f));
-            Vector3 v = Spawner.position;
-            Instantiate(enemy,v ,Quaternion.identity);
+            Instantiate(enemy,Spawner.position ,transform.rotation);
         }
     }
     void FixedUpdate()
     {
-        
+        Spawner.position += velocity * Time.deltaTime;
+        transform.LookAt(player.position);
+        if (Spawner.position.z >= 30)
+        {
+            z = velocity.z;
+            velocity = new Vector3(velocity.x, 0, -velocity.z);
+        }
+        if (Spawner.position.z <= -30)
+        {
+            velocity = new Vector3(velocity.x, 0, z);
+        }
+
+        Spawner.position += velocity * Time.deltaTime;
+        if (Spawner.position.x >= 54)
+        {
+            x = velocity.x;
+            velocity = new Vector3(-velocity.x, 0, velocity.z);
+        }
+        if (Spawner.position.x <= -54)
+        {
+            velocity = new Vector3(x, 0, velocity.z);
+        }
     }
 }
