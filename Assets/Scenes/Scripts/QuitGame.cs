4bd40129f4c037cd744bd60cc0cabe21938c56db
Elitetykoro <tykoromathijssen@gmail.com>
2024-03-08 14:26:12 +0100

    MainMenuGameMusicMenuMusicMainMenuLogo

--- Assets/Scenes/Scripts/QuitGame.cs
+++ Assets/Scenes/Scripts/QuitGame.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
 
 public class QuitGame : MonoBehaviour
 {
+    Vector3 Balloon = new Vector3(0,18,35);
+    [SerializeField] Transform transform;
+    private float speed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,22 @@ public class QuitGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Balloon;
+        Balloon.y += speed;
     }
     public void Quit()
     {
         Application.Quit();
         //UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void startGame()
+    {
+        StartCoroutine("_QuitGame");
+    }
+    public IEnumerator _QuitGame()
+    {
+        speed = 4;
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("lazers",LoadSceneMode.Single);
     }
 }
